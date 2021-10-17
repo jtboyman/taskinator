96e4e783b654697a307c852c32f92b204e664a9a
Edelgard <jtboyman@gmail.com>
2021-10-16 22:55:21 -0400

    working and styled, tasks can be added with names and types

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,24 @@ let createTaskHandler = function(event) {
 
     event.preventDefault();
 
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
     let listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    //create div to hold task info and add to list item
+    let taskInfoEl = document.createElement("div");
+    //give it a class name
+    taskInfoEl.ELEMENT_NODE.className = "task-info";
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+    
+    //add entire list item to the list
     tasksToDoEl.appendChild(listItemEl);
 }
 
